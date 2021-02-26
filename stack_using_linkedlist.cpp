847806f808186f731acfe8d8e67ee81024f18d50
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2021-02-26 17:12:42 +0200

    Create stack_using_linkedlist.cpp

---
+++ stack_using_linkedlist.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+	int value;
+	Node* next;
+};
+
+class Stack {
+
+	Node* head = nullptr;
+
+public:
+	void push(int val) {
+		if (!isFull()) {
+			Node* temp = new Node();
+			temp->value = val;
+			temp->next = head;
+			head = temp;
+		}
+	}
+int pop() {
+	if (!isEmpty()) {
+		Node* temp = head;
+		head = head->next;
+		int val = temp->value;
+		delete(temp);
+		return val;
+	}
+}
+
+void display() {
+	Node* temp = head;
+	while (temp) {
+		cout << temp->value << " ";
+		temp = temp->next;
+	}
+}
+
+bool isEmpty() {
+	return head == nullptr;
+}
+bool isFull() {
+	return false;
+}
+void clear() {
+	Node* temp = head->next;
+	Node* prev = head;
+	while (temp) {
+		delete(prev);
+		prev = temp;
+		temp = temp->next;
+	}
+	delete(prev);
+}
+
+};
+
+int main() {
+	Stack s;
+	for (int i = 0; i <= 10; i++) {
+		s.push(i);
+	}
+	s.display();
+	s.clear(); 
+	return 0;
+}
